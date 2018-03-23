6a066a4e3c3db5a7e6f6a7c5327a095c5cd5e372
dainvinc <vishal.koosuri1@marist.edu>
2018-03-23 01:16:01 -0400

    Added more endpoints to test out patient functioning page

--- src/main/java/com/pfizer/controllers/MainController.java
+++ src/main/java/com/pfizer/controllers/MainController.java
@@ -1,30 +1,23 @@
 package com.pfizer.controllers;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.pfizer.models.Patient;
-import com.pfizer.models.Sample;
 import com.pfizer.repositories.PatientRepository;
-import com.pfizer.repositories.SampleRepository;
 
 @Controller
 public class MainController {
 	
 	@Autowired
-	SampleRepository sampleRepository;
+	PatientRepository patientRepository;
 	
 	@GetMapping("/")
 	public String homePage() {
@@ -41,20 +34,40 @@ public class MainController {
 	@GetMapping("/patient")
 	public String patientHomePage(Model model) {
 		
-		model.addAttribute("patient", new Patient());
+//		model.addAttribute("patient", new Patient());
 		return "patient/patienthome";
 	}
 	
-	@PostMapping("/patient")
-	public String postNewPatient(@ModelAttribute Patient patient) {
-
-		return "result";
+	@GetMapping("/patient/new")
+	public String getAllPatientsPage(Model model) {
+		
+		return "patient/signup";
 	}
 	
-//	@GetMapping("/result") 
-//	public String getResutlspage() {
-//		return "result";
-//	}
+	@PostMapping("/patient/add")
+	public String postNewPatient(@ModelAttribute Patient patient) {
+		
+		patientRepository.save(patient);
+		return "patient/all";
+	}
+	
+	@GetMapping("/patient/all")
+	public String allPatientsPage(@ModelAttribute Patient patient, Model model) {
+		
+		List<Patient> patients = (ArrayList<Patient>) patientRepository.findAll();
+		
+		model.addAttribute("patients", patients);
+//		return new ResponseEntity<>(new Patient(
+//				patient.getPatient_id(), 
+//				patient.getFirst_name(), 
+//				patient.getLast_name(), 
+//				patient.getCity(),
+//				patient.getCountry(),
+//				patient.getPhoneNo(), 
+//				patient.getDob(), 
+//				patient.getPayments()), HttpStatus.OK);
+		return "patient/all";
+	}
 	
 	@GetMapping("/signup")
 	public String signUpPage() {
@@ -62,21 +75,21 @@ public class MainController {
 		return "signup";
 	}
 	
-	@GetMapping("/patients")
-	public String getAllPatients(@ModelAttribute("sample") Sample sample, Model model) {	
-
-		List<Sample> samples = (List<Sample>) sampleRepository.findAll();
-		model.addAttribute("samples", samples);
-		return "simple";
-	}
-	
-	@PostMapping("/patients")
-	public ResponseEntity<Sample> saveNewPatient(@ModelAttribute("sample") Sample sample, Model model) {
-		System.out.println(sample.getId()+"***********" +sample.getName());
-		sampleRepository.save(sample);
-//		ModelAndView mv = new ModelAndView("simple");
-		
-		return new ResponseEntity<>(new Sample(sample.getId(), sample.getName(), sample.getPrice()), HttpStatus.OK);
-	}
+//	@GetMapping("/patients")
+//	public String getAllPatients(@ModelAttribute("sample") Sample sample, Model model) {	
+//
+//		List<Sample> samples = (List<Sample>) sampleRepository.findAll();
+//		model.addAttribute("samples", samples);
+//		return "simple";
+//	}
+//	
+//	@PostMapping("/patients")
+//	public ResponseEntity<Sample> saveNewPatient(@ModelAttribute("sample") Sample sample, Model model) {
+//		System.out.println(sample.getId()+"***********" +sample.getName());
+//		sampleRepository.save(sample);
+////		ModelAndView mv = new ModelAndView("simple");
+//		
+//		return new ResponseEntity<>(new Sample(sample.getId(), sample.getName(), sample.getPrice()), HttpStatus.OK);
+//	}
 	
 }
