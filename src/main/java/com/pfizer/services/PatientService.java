f74fa7da05ededa03806b09b4bf5cebcd85e59b2
dainvinc <vishal.koosuri1@marist.edu>
2018-03-24 20:02:29 -0400

    Commented out patient service temporarily

--- src/main/java/com/pfizer/services/PatientService.java
+++ src/main/java/com/pfizer/services/PatientService.java
@@ -1,34 +1,34 @@
-package com.pfizer.services;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.pfizer.models.Patient;
-import com.pfizer.repositories.PatientRepository;
-
-@Service("patientService")
-public class PatientService {
-	
-	@Autowired
-	private PatientRepository patientRepository;
-	
-	public List<Patient> getAllPatients() {
-		List<Patient> patients = new ArrayList<>();
-		
-		patientRepository.findAll()
-			.forEach(patients::add);
-		
-		return patients;
-	}
-	
-//	public Patient findPatientById(Long id) {
-//		return patientRepository.findById(id);
+//package com.pfizer.services;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import com.pfizer.models.Patient;
+//import com.pfizer.repositories.PatientRepository;
+//
+//@Service("patientService")
+//public class PatientService {
+//	
+//	@Autowired
+//	private PatientRepository patientRepository;
+//	
+//	public List<Patient> getAllPatients() {
+//		List<Patient> patients = new ArrayList<>();
+//		
+//		patientRepository.findAll()
+//			.forEach(patients::add);
+//		
+//		return patients;
 //	}
-	
-	public void savePatient(Patient patient) {
-		patientRepository.save(patient);
-	}
-}
+//	
+////	public Patient findPatientById(Long id) {
+////		return patientRepository.findById(id);
+////	}
+//	
+//	public void savePatient(Patient patient) {
+//		patientRepository.save(patient);
+//	}
+//}
